f64348446126aca1c74f9c3e8386487f2bbc6539
Akhier Dragonheart <akherthedragonhearted@gmail.com>
2017-07-27 18:57:08 -0400

    Add 3 test components to test_manager
    compA has a and b with -1 and 1 respectively
    compB has t and f with True and False respectively
    and compC has l and r with 'left' and 'right' respectively

---
+++ tests/test_manager.py
@@ -0,0 +1,14 @@
+class compA:
+    def __init__(self):
+        self.a = -1
+        self.b = 1
+
+class compB:
+    def __init__(self):
+        self.t = True
+        self.f = False
+
+class compC:
+    def __init__(self):
+        self.l = 'left'
+        self.r = 'right'
