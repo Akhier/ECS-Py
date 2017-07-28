9197b944affa06f0fc0d5eb4da511f10fad93c89
Akhier Dragonheart <akherthedragonhearted@gmail.com>
2017-07-27 23:29:29 -0400

    Add main function to console_example.py

--- examples/console_example.py
+++ examples/console_example.py
@@ -1,4 +1,5 @@
 import ecs
+import time
 
 
 class Coordinate:
@@ -23,3 +24,30 @@ class ProcessMovement(ecs.SystemTemplate):
             coordinate.x += velocity.x
             coordinate.y += velocity.y
             print("Current Location: {}".format((coordinate.x, coordinate.y)))
+
+
+def main():
+    # Creates the Manager
+    manager = ecs.Manager()
+
+    # Instantiates the ProcessMovement System and adds it to manager
+    processmovement = ProcessMovement()
+    manager.add_system(processmovement)
+
+    # Create an Entity with a Component then adds another Component
+    entity = manager.new_entity(Coordinate(x=0, y=0))
+    manager.add_component_to_entity(Velocity(x=1, y=2), entity)
+
+    # Basic Main Loop
+    try:
+        while True:
+            # Call manager.process() to run all the systems
+            manager.process()
+            time.sleep(1)
+    except KeyboardInterrupt:
+        return
+
+
+if __name__ == '__main__':
+    print("example - press ctrl+C to quit")
+    main()
