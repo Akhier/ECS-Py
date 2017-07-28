53ff1862380a303cc38cca902517a8a9cb7a700a
Akhier Dragonheart <akherthedragonhearted@gmail.com>
2017-07-27 23:08:48 -0400

    Add 2 Component classes to console_example.py

---
+++ examples/console_example.py
@@ -0,0 +1,10 @@
+class Coordinate:
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+
+
+class Velocity:
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
